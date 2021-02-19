6a7c3086e12a4fa716647b1b40adbd615eabeb4d
mq <luumanhquan.91@gmail.com>
2021-02-19 20:03:19 +0700

    .

--- src/components/admin/menu.js
+++ src/components/admin/menu.js
@@ -1,16 +1,5 @@
 import React, { Component } from "react";
-import {
-  NavLink,
-  BrowserRouter as Router,
-  Redirect,
-  Route,
-  Switch,
-} from "react-router-dom";
-import News from "./news";
-import Tags from "./tags";
-import NewTag from "./new-tag";
-import Feedbacks from "./feedbacks";
-import Users from "./users";
+import { NavLink, Redirect } from "react-router-dom";
 class Menu extends Component {
   constructor(props) {
     super(props);
@@ -33,46 +22,33 @@ class Menu extends Component {
       return <Redirect to="/" />;
     }
     return (
-      <Router>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-              <NavLink to="/admin" className="nav-item nav-link active">
-                Home <span class="sr-only">(current)</span>
-              </NavLink>
-              <NavLink to="/admin/news" className="nav-item nav-link active">
-                News
-              </NavLink>
-              <NavLink to="/admin/tags" className="nav-item nav-link active">
-                Tags
-              </NavLink>
-              <NavLink to="/admin/new-tag" className="nav-item nav-link active">
-                New-Tag
-              </NavLink>
-              <NavLink
-                to="/admin/feedbacks"
-                className="nav-item nav-link active"
-              >
-                FeedBacks
-              </NavLink>
-              <NavLink to="/admin/users" className="nav-item nav-link active">
-                Users
-              </NavLink>
-            </div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <NavLink to="/admin" className="nav-item nav-link active">
+              Home <span class="sr-only">(current)</span>
+            </NavLink>
+            <NavLink to="/admin/news" className="nav-item nav-link active">
+              News
+            </NavLink>
+            <NavLink to="/admin/tags" className="nav-item nav-link active">
+              Tags
+            </NavLink>
+            <NavLink to="/admin/new-tag" className="nav-item nav-link active">
+              New-Tag
+            </NavLink>
+            <NavLink to="/admin/feedbacks" className="nav-item nav-link active">
+              FeedBacks
+            </NavLink>
+            <NavLink to="/admin/users" className="nav-item nav-link active">
+              Users
+            </NavLink>
           </div>
-          <button type="button" onClick={this.logout}>
-            Logout
-          </button>
-        </nav>
-        <Switch>
-          <Route exact path="/admin" />
-          <Route path="/admin/news" component={News}></Route>
-          <Route path="/admin/tags" component={Tags}></Route>
-          <Route path="/admin/new-tag" component={NewTag}></Route>
-          <Route path="/admin/feedbacks" component={Feedbacks}></Route>
-          <Route path="/admin/users" component={Users}></Route>
-        </Switch>
-      </Router>
+        </div>
+        <button type="button" onClick={this.logout}>
+          Logout
+        </button>
+      </nav>
     );
   }
 }
--- src/components/router.js
+++ src/components/router.js
@@ -4,17 +4,26 @@ import Login from "../login";
 import Signup from "../signup";
 import NotFound from "./not_found";
 
+import Main from "./main";
+
 import View from "./client/menu/view";
 import Favourite from "./client/menu/favourite";
 import Blog from "./client";
 import NewDetail from "./client/newDetail";
+
+import News from "./admin/news";
+import Tags from "./admin./tags";
+import NewTag from "./admin./new-tag";
+import Feedbacks from "./admin./feedbacks";
+import Users from "./admin./users";
+import Admin from "./admin";
 
 class Router extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/"></Route>
+          <Route exact path="/" component={Main}></Route>
           <Route path="/login" component={Login}></Route>
           <Route path="/signup" component={Signup}></Route>
           <Route component={NotFound}></Route>
@@ -23,6 +32,13 @@ class Router extends Component {
           <Route path="/blog/view" component={View} />
           <Route path="blog/favourite" component={Favourite} />
           <Route path="/blogs/news/:id" component={NewDetail} />
+
+          <Route exact path="/admin" component={Admin} />
+          <Route path="/admin/news" component={News}></Route>
+          <Route path="/admin/tags" component={Tags}></Route>
+          <Route path="/admin/new-tag" component={NewTag}></Route>
+          <Route path="/admin/feedbacks" component={Feedbacks}></Route>
+          <Route path="/admin/users" component={Users}></Route>
         </Switch>
       </BrowserRouter>
     );
